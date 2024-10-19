4a48a3c8249083ea412b2449a20cba3727a23eea
Siddharth Saraf <saraf.siddharth@gmail.com>
2024-10-19 16:50:34 -0700

    Added basic hike selection and trail display functionality

--- src/HikeSelect.js
+++ src/HikeSelect.js
@@ -1,11 +1,65 @@
-import React from 'react'
-import { Link } from 'react-router-dom'
+import React, { useState, useEffect } from 'react'
+import { Link, navigate } from 'react-router-dom'
 
 function HikeSelect () {
+	const [trailName, setTrailName] = useState("");
+	const [trailData, setTrailData] = useState({});
+	const [trails, setTrails] = useState([]);
+	const [filteredTrails, setFilteredTrails] = useState([]);
+	const [search, setSearch] = useState("");
+
+	useEffect(() => {
+		fetch("http://localhost:3000/api/trails")
+		.then(resp => resp.json() )
+		.then(data => {
+		    if (!data) throw new Error('No data received');
+		    if (data==undefined) console.log("data undefined");
+	    	setTrails(data);
+  		})
+		.catch(err => console.log("Error connecting to server: ", err));
+	}, []);
+
+	const searchHike = () => {
+		fetch("http://localhost:3000/api/trail?name=" + encodeURI(trailName))
+		.then(resp => resp.json() )
+		.then(data => {
+		    if (!data) throw new Error('No data received');
+		    if (data==undefined) console.log("data undefined");
+		    if (data.trail != undefined) {
+		    	setTrailData(data.trail);
+		    	setTrailName(data.trail.properties.TRAIL_NAME);
+		    };
+  		})
+		.catch(err => console.log("Error connecting to server: ", err));
+	}
+
+	const handleSearch = (e) => {
+		const val = e.target.value;
+		setSearch(val);
+	}
+
+	useEffect(() => {
+		setFilteredTrails(trails.filter(trail => 
+			trail.toLowerCase().includes(search.toLowerCase())
+		));
+	}, [search])
 
 	return (
 		<div>
-			<Link to="/map">Map</Link>
+			<Link to="/map" state={{ trailData }} >Open Map</Link>
+			<input 
+		        type="text" 
+		        value={search} 
+		        onChange={handleSearch} 
+		        placeholder="Search trails..."
+	      	/>
+	      	<ul>
+	        	{filteredTrails.map((trail, index) => (
+	          		<li key={index}><button onClick={() => {setTrailName(trail)}}>{trail}</button></li>
+	        	))}
+	      	</ul>
+			<button onClick={searchHike}>Search Hike</button>
+			<p>{trailData == {} ? "" : trailName}</p>
 		</div>
 	)
 
--- src/Map.js
+++ src/Map.js
@@ -1,13 +1,20 @@
 import React, { useState, useEffect } from 'react';
-import { MapContainer, TileLayer, Marker, Popup, CircleMarker } from 'react-leaflet';
+import { MapContainer, TileLayer, Marker, Popup, CircleMarker, Polyline } from 'react-leaflet';
+import { useLocation } from 'react-router-dom';
 import 'leaflet/dist/leaflet.css';
 
 function Map () {
+  const routeLocation = useLocation();
+  const { trailData } = routeLocation.state || {};
   const [location, setLocation] = useState({ latitude: null, longitude: null });
   const [error, setError] = useState(null);
+  const [coordinates, setCoordinates] = useState(null);
 
   useEffect(() => {
-    if (navigator.geolocation) {
+    if (trailData != null) {
+      console.log("got trail data: ", trailData);
+      setCoordinates(trailData.geometry.coordinates[0].map(([a, b]) => [b, a]));
+    } else if (navigator.geolocation) {
       navigator.geolocation.getCurrentPosition(
         (position) => {
           setLocation({
@@ -21,8 +28,14 @@ function Map () {
       );
     } else {
       setError("Geolocation is not supported by this browser.");
-    }
-  }, []);
+    };
+  }, [trailData]);
+
+  useEffect(() => {
+      if (coordinates != null) {
+        setLocation({ latitude: coordinates[0][0], longitude: coordinates[0][1]});
+      };
+  }, [coordinates]);
 
   return (
     <div>
@@ -35,6 +48,7 @@ function Map () {
           <CircleMarker center={[location.latitude, location.longitude]} pathOptions={{ color: 'red' }} radius={20}>
             <Popup>You are here</Popup>
           </CircleMarker>
+          { trailData ? <Polyline pathOptions={{ color: "red" }} positions={coordinates} /> : <h1>"No trail loaded"</h1>}
         </MapContainer>
       ) : (
         <p>{error ? `Error: ${error}` : "Loading..."}</p>
